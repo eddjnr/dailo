d52d72e88b79ea213cc4ca14b73c244678f0d5a7
Junior Albuquerque <junioralbuquerque81@gmail.com>
2026-02-05 22:24:01 -0300

    feat: remove unused hook exports for cleaner component index

--- components/index.ts
+++ components/index.ts
@@ -14,8 +14,6 @@ export { EventDialog } from "./event-dialog";
 export { EventItem } from "./event-item";
 export { EventsPopup } from "./events-popup";
 // Hook exports
-export * from "./hooks/use-current-time-indicator";
-export * from "./hooks/use-event-visibility";
 export { MonthView } from "./month-view";
 // Type exports
 export type { CalendarEvent, CalendarView, EventColor } from "./types";
